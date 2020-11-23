b915388a8b7d2d45a678b9757ff0967393e67605
ghinafirdaus <tajghinaqf@gmai.com>
2020-11-23 17:34:51 +0700

    Added Header logo, navbar, but button still on progress

---
+++ components/Button.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <button class="button--green">Sign In</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  color: #fff;
+  background-color: #41AEA9;
+  text-decoration: none;
+  width: 180px;
+  height: 50px;
+  border: 0;
+  margin-left: 100px;
+}
+
+.button--green:hover {
+  background: white;
+  border: 1px solid #41AEA9;
+  color: #41AEA9;
+}
+</style>
